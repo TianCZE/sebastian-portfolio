fae5d168089eb05f60936dc218f20876d6821df2
TianCZE <basty.novak@seznam.cz>
2023-08-19 08:57:48 +0200

    Basic Layout finished
    Added background, some text, basic card for showing of my projects (with ripple effect O.o).

---
+++ src/components/Card.tsx
@@ -0,0 +1,38 @@
+"use client";
+import {TERipple} from "tw-elements-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Card({img = '/Owl.png'}) {
+    return (
+        <div
+            className="relative self-center object-center rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] max-w-[500px] dark:bg-neutral-700">
+            <div className="relative rounded-t-lg overflow-hidden bg-cover bg-no-repeat max-h-[500px] place-items-center">
+                <TERipple>
+                    <Image
+                        className={'aspect-square'}
+                        src={img}
+                        alt=""
+                        width={500}
+                        height={500}
+                        style={{objectFit: 'cover'}}
+                    />
+                    <Link href="#!">
+                        <div
+                            className=" absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+                    </Link>
+                </TERipple>
+            </div>
+            <div className="p-6">
+                <h5
+                    className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                    Card title
+                </h5>
+                <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card`s content.
+                </p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Experiment.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import background from "../../public/background.png"
+import {TERipple} from "tw-elements-react";
+
+function Experiment() {
+    return(
+        <>
+            <TERipple>
+                <button>
+                    Sup
+                </button>
+            </TERipple>
+        </>
+
+
+    )
+}
+
+export default Experiment;
+
+
+
+// <div className="grid justify-center place-content-center bg-opacity-50 object-fill w-full h-full bg-cover shadow-lg backdrop-blur-md">
+//
+// </div>
+
+
+// layout={'fill'}
+// objectFit={'cover'}
+// objectPosition={'center'}
